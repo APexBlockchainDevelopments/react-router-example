bcc187336b9e23c026cd21a3e9d38b259fd10d5b
APex <37938428+APexBlockchainDevelopments@users.noreply.github.com>
2023-03-22 14:09:04 -0600

    added new updates

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {Link, BrowserRouter, Routes, Route} from 'react-router-dom'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/about'>About</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/about" element={<About/>}/>
+          <Route path="/" element={<Home/>}/>
+          <Route path="*" element={<NotFound/>}/>
+        </Routes>
+
+      </div>
+    </BrowserRouter>
   );
+}
+
+function Home(){
+  return(
+    <h2>Home</h2>
+  )
+}
+
+function About(){
+  return(
+    <h2>About</h2>
+  )
+}
+
+function NotFound(){
+  return (
+    <h2>Page Not Found :(</h2>
+  )
 }
 
 export default App;
